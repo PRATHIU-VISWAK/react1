1c93e0b263fe1dcece915c5bf9b3bb22c878076b
PRATHIUVISWAK <prathiuviswakdev@gmail.com>
2024-03-02 03:19:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import { planets } from "./Planets";
 
 function App() {
-  return <div className="App"> Hello World </div>;
+  return (
+    <div className="App">
+      {planets.map(
+        (planet, key) => !planet.isGasPlanet && <h1> {planet.name} </h1>
+      )}
+    </div>
+  );
 }
 
 export default App;
