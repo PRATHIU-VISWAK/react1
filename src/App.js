b7fc179e945ef8d5b93efa9dd301ccebb4b8f51b
PRATHIUVISWAK <prathiuviswakdev@gmail.com>
2024-03-07 14:13:38 +0530

    useQuery

--- src/App.js
+++ src/App.js
@@ -6,14 +6,16 @@ import { Contacts } from "./pages/Contact";
 import { Items } from "./pages/Items";
 import { Profile } from "./pages/Profile";
 import { NAVBAR } from "./components/NAVBAR";
+import { QueryClient , QueryClientProvider  } from "@tanstack/react-query";
 
 export const AppContext = createContext()
 function App() {
-  
+  const query = new QueryClient()
   const [usernames , setUsername] = useState("Viswak")
   return (
     <AppContext.Provider value = {{usernames , setUsername}}>
     <div className="App">
+      <QueryClientProvider client={query}>
       <Router>
         <div><NAVBAR /></div>
         <Routes>
@@ -23,6 +25,7 @@ function App() {
           <Route path="/Profile" element={<Profile />} />
         </Routes>
       </Router>
+      </QueryClientProvider>
     </div>
     </AppContext.Provider>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
 import { useContext } from "react"
 import { AppContext } from "../App"
+import { useQuery } from "@tanstack/react-query"
+import Axios from "axios"
 
 export const Home = () =>{
+    const {data} = useQuery({ 
+        queryKey : ["cat"] , 
+        queryFn :  () => {
+       return Axios.get("https://catfact.ninja/fact").then((res) => {
+        return res.data })
+    }})
     const {usernames} = useContext(AppContext)
+    //console.log(data?.fact)
     return(
         <div>
             <h1>Home : the user is {usernames}</h1>
+            <h2>{data?.fact}</h2>
         </div>
     )
 }
