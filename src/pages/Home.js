dc2573f4e696249c65182db7c74030b0f32a054b
PRATHIUVISWAK <prathiuviswakdev@gmail.com>
2024-03-07 21:06:19 +0530

    useQuery

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { useQuery } from "@tanstack/react-query"
 import Axios from "axios"
 
 export const Home = () =>{
-    const {data} = useQuery({ 
+    const {data , isLoading , refetch } = useQuery({ 
         queryKey : ["cat"] , 
         queryFn :  () => {
        return Axios.get("https://catfact.ninja/fact").then((res) => {
@@ -12,10 +12,12 @@ export const Home = () =>{
     }})
     const {usernames} = useContext(AppContext)
     //console.log(data?.fact)
+    if (isLoading) return <h2>Loading</h2>
     return(
         <div>
             <h1>Home : the user is {usernames}</h1>
             <h2>{data?.fact}</h2>
+            <button onClick={refetch}>update data</button>
         </div>
     )
 }
